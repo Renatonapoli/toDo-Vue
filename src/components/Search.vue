d33906ddbbac8eb8f38c9c6902eb6e0e47f54003
Renato Napoli <renato.napoli@nagro.app>
2023-06-20 15:43:41 -0300

    Feat: Layout componente Search

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,7 +1,10 @@
 <template>
-  <div>
-    <h1>Search</h1>
-  </div>
+  <v-flex class="container">
+    <input type="text" />
+    <button>
+      Criar <img src="@/assets/addList.svg" alt="Icone de do botão criar" />
+    </button>
+  </v-flex>
 </template>
 
 <script lang="ts">
@@ -13,3 +16,52 @@ export default defineComponent({
   // setup() {},
 });
 </script>
+
+<style scoped>
+.container {
+  display: flex;
+  margin-top: -1.8rem;
+  position: relative;
+  justify-content: center;
+  align-items: center;
+}
+
+.container input {
+  width: 39.875rem;
+  height: 3.375rem;
+  background: var(--gray-500);
+  border: 1px solid var(--gray-700);
+  border-radius: 8px;
+  color: var(--gray-100);
+  padding: 1rem;
+}
+.container input:focus {
+  outline: transparent;
+  box-shadow: 0 0 0 2px var(--blue-dark);
+}
+
+.container button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 0.5rem;
+  width: 5.625rem;
+  height: 3.25rem;
+  background: var(--blue-dark);
+  color: var(--gray-100);
+  font-weight: 700;
+  font-size: 0.875rem;
+  line-height: 140%;
+  gap: 0.5rem;
+  border-radius: 0.5rem;
+  border: none;
+  cursor: pointer;
+
+  transition: 0.8s linear;
+}
+
+.container button:hover {
+  background: var(--purple-dark);
+  transition-delay: 12ms linear;
+}
+</style>
