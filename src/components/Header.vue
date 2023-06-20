3d637cbc1b9b0eb6c3d66ae3bc5e5de6c592dc11
Renato Napoli <renato.napoli@nagro.app>
2023-06-20 15:40:40 -0300

    Feat: Laout componente Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-    <h1>Navbar</h1>
-  </div>
+  <v-flex class="logo">
+    <img src="@/assets/logo.svg" alt="logo" />
+  </v-flex>
 </template>
 
 <script lang="ts">
@@ -12,3 +12,18 @@ export default defineComponent({
   // setup() {},
 });
 </script>
+
+<style scope>
+.logo {
+  display: flex;
+  justify-content: center;
+  padding: 5rem 0;
+  height: 200px;
+  background: var(--gray-700);
+}
+
+.logo img {
+  width: 7.92rem;
+  height: 2.9rem;
+}
+</style>
